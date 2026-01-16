30d8bf6d0a93d999af20c8195123ba8f087bda7e
tribez1927-bit <tribez1927@gmail.com>
2026-01-16 16:24:17 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const WebSocket = require('ws');
 
 // Use Render's Environment Port (Critical for deployment)
-const PORT = process.env.PORT || 3535;
+const PORT = process.env.PORT || 8080;
 const wss = new WebSocket.Server({ port: PORT });
 
 const rooms = new Map();
